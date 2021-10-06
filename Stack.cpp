cc0a99e68bc3dc262d12f14d9cf5ca0344f8394a
Imtiaj Ahmed Anik <87453627+ImtiajAhmedAnik@users.noreply.github.com>
2021-10-06 20:29:16 +0000

    Change

--- Stack.cpp
+++ Stack.cpp
@@ -1,66 +1,87 @@
 #include <iostream>
 using namespace std;
-class stack{
+class Stack {
 public:
-    int arr[10];
-    int size=sizeof (arr)/sizeof(arr[0]);
-    int counter=-1;
+    int stack[10],top=-1;
+    int size=sizeof(stack)/sizeof(stack[0]);
+    bool isFull(){
+        if(top==size-1){
+            return true;
+        }
+        return false;
+    }
+    bool isEmpty(){
+        if(top==-1)
+            return true;
+        else
+            return false;
+    }
+    void Push(int value){
+        if(isFull()){
+            cout <<"\nQueue is Full!!\n";
+            return;
+        }
+        top++;
+        stack[top]=value;
+        cout<<"\nInsert -> "<<value<<"\n";
+    }
+    void Pop(){
+        if(isEmpty()){
+            cout<<"\nQueue is Empty!!\n";
+            return;
+        }
+        int val=stack[top];
+        cout <<"\nDeleted -> "<<val<<"\n";
+        top--;
 
-    void push(int val){
-        if (counter<size-1){
-            counter++;
-            arr[counter]=val;
-            cout<<"Push Item: "<<arr[counter]<<"\n";
-        } else{
-            cout<<"Stack Full\n";
-        }
     }
-    void pop(){
-        if(counter>=0){
-            cout<<arr[counter]<<" Delete Successfully\n";
-            counter--;
-        } else{
-            cout<<"Empty Stack!\n";
+    int Peek(){
+        if(isEmpty()){
+            cout<<"\nQueue is Empty!!\n";
+            return 1;
         }
+        return stack[top];
     }
-    int peek(){
-        if(counter>=0){
-            return arr[counter];
+
+    void showStack()
+    {
+        if(isEmpty()) {
+            cout<<"\nStack is Empty!!\n";
+            return;
         }
-        cout<<"Empty Stack\n";
-    }
-    void showall(){
-        for (int i = 0; i <=counter; i++) {
-            cout<<arr[i]<<" ";
+        cout<<"Stack Element: ";
+        for(int i=0;i<=top;i++) {
+            cout<<stack[i]<<" ";
         }
-        cout<<"\n";
+        cout<<"\n\n";
     }
 };
 int main() {
-    stack Obj;
+    Stack Obj;
     bool choice=true;
     while(choice){
+        cout<<"\n.....Stack.....\n";
         cout<<"Push Element Press-1\n";
         cout<<"Pop Element Press-2\n";
         cout<<"Peek Element Press-3\n";
         cout<<"ShowAll Press-4\n";
-        cout<<"Exit Than Press-5\n\n";
+        cout<<"Exit Than Press-5\n";
         int Case,val;
         cin>>Case;
         switch (Case) {
             case 1:
                 cout<<"Enter Value: ";
                 cin>>val;
-                Obj.push(val);
+                Obj.Push(val);
                 break;
             case 2:
-                Obj.pop();
+                Obj.Pop();
                 break;
             case 3:
-                Obj.peek();
+               cout<<"\nTop Element Stack -> "<<Obj.Peek()<<"\n";
                 break;
             case 4:
-                Obj.showall();
+                Obj.showStack();
                 break;
             case 5:
                 choice= false;
@@ -68,5 +89,4 @@ int main() {
                 cout<<"Please Try Again! Press Enter (1-5)\n\n";
         }
     }
-
 }
